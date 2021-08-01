5b421dad1c413f945d610002c9849646c1a4a616
Turkibash <dev@turki.tech>
2021-08-01 23:06:36 +0300

    Add project files.

---
+++ EmployeeAPI/Models/Employee.cs
@@ -0,0 +1,10 @@
+﻿namespace EmployeeAPI.Models
+{
+    public class Employee
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public bool IsAttended { get; set; }
+    }
+}
